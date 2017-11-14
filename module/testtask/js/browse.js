b40265357dc7132b4ba775f57c61dc38e9933ad0
psyafter <psyafter@gmail.com>
2017-11-14 13:37:13 +0200

    clean

--- module/testtask/js/browse.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function()
-{
-    if(flow == 'onlyTest')
-    {
-        $('#modulemenu > .nav > li').removeClass('active');
-        $('#modulemenu > .nav > li[data-id=' + status + ']').addClass('active');
-    }
-});
